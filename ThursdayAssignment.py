2ddf1aaa2d805363a4fe42217b76c3b8ccf7c3d8
codemyburger <ff7yeah@yahoo.com>
2019-04-22 08:58:57 -0500

    Add files via upload

---
+++ ThursdayAssignment.py
@@ -0,0 +1,123 @@
+# Assignment 1
+# def hello (name):
+#     print("Hello " + name)    
+
+# hello("David")
+
+# Assignment 2
+# import matplotlib.pyplot as plot 
+
+# def f(x): 
+#      # put your code here
+#      x = x + 1
+#      return(x)
+
+# xs = list(range(-3, 4)) 
+# ys = [] 
+
+# for x in xs: 
+#      ys.append(f(x)) 
+
+# plot.plot(xs, ys) 
+# plot.show()
+
+#Assignment 3
+# import matplotlib.pyplot as plot 
+
+# def f(x):
+#     y = x ** 2
+#     return(y)
+
+# xs = list(range(-100, 101))
+# ys = []
+
+# for x in xs:
+#     ys.append(f(x))
+
+# plot.plot(xs, ys)
+# plot.show()
+
+# Assignment 4
+# import matplotlib.pyplot as plot 
+
+# def f(x):
+#     if x % 2 == 1:
+#         return 1
+#     else:
+#         return -1
+#     y = x + 1
+#     return(x)
+
+# xs = list(range(-5, 6))
+# ys = []
+
+# for x in xs:
+#     ys.append(f(x))
+
+# plot.bar(xs, ys)
+# plot.show()
+
+# Assignment 5
+# import matplotlib.pyplot as plot 
+# import math
+
+# def f(x):
+#     return math.sin(x)
+
+# xs = list(range(-5, 6))
+# ys = []
+
+# for x in xs:
+#     ys.append(f(x))
+
+# plot.plot(xs, ys)
+# plot.show()
+
+# Assignment 6
+# from numpy import arange
+
+# import matplotlib.pyplot as plot 
+# import math
+
+# def f(x):
+#     return math.sin(x)
+
+# xs = arange(-5, 6, 0.1)
+# ys = []
+
+# for x in xs:
+#     ys.append(f(x))
+
+# plot.plot(xs, ys)
+# plot.show()
+
+# Assignment 7
+# import matplotlib.pyplot as plot 
+
+# def celcius(c):
+#     f = (c * 1.8) + 32
+#     return(f)
+    
+# xs = list(range(-5, 6))
+# ys = []
+
+# for x in xs:
+#     ys.append(celcius(f))
+
+# plot.plot(xs, ys)
+# plot.show()
+
+# Assignment 8
+# def again(x):
+#     answer = input("Do you want to play again?: ")
+#     if answer.lower() == "Yes":
+#         return True
+#     else:
+#         return False
+    
+    
+# print(again)
+
+
+
+
---
+++ Turtle.py
@@ -0,0 +1,98 @@
+# Exercise 1-1
+# from turtle import *
+
+# left(60)
+# forward(100)
+# right(120)
+# forward(100)
+# right(120)
+# forward(100)
+
+# mainloop()
+
+# Exercise 1-2
+# from turtle import *
+
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+
+
+# mainloop()
+
+# Exercise 1-3
+# from turtle import *
+
+# right(45)
+# forward(100)
+# right(60)
+# forward(100)
+# right(75)
+# forward(100)
+# right(70)
+# forward(100)
+# right(70)
+# forward(120)
+
+
+# mainloop()
+
+
+# Exercise 1-4
+# from turtle import *
+
+# forward(100)
+# left(60)
+# forward(100)
+# left(60)
+# forward(100)
+# left(60)
+# forward(100)
+# left(60)
+# forward(100)
+# left(60)
+# forward(100)
+
+# mainloop()
+
+# Exercise 1-5
+# from turtle import *
+
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+
+# mainloop()
+
+# Exercise 1-6
+# from turtle import*
+
+# forward(100)
+# right(145)
+# forward(100)
+# right(145)
+# forward(100)
+# right(145)
+# forward(100)
+# right(145)
+# forward(100)
+
+# mainloop()
